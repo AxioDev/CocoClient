7c79a153d5e9d5af147f6e20b07aa34b5fd79628
Thibault Barillet <axiocontactezmoi@protonmail.com>
2024-07-04 15:26:48 +0200

    spa

--- next.config.mjs
+++ next.config.mjs
@@ -2,22 +2,23 @@
 import path from 'path';
 
 const nextConfig = {
-  reactStrictMode: false,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            icon: true,
-          },
-        },
-      ],
-    });
+    output: 'export',
+    reactStrictMode: false,
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: [
+                {
+                    loader: '@svgr/webpack',
+                    options: {
+                        icon: true,
+                    },
+                },
+            ],
+        });
 
-    return config;
-  },
+        return config;
+    },
 };
 
 export default nextConfig;
